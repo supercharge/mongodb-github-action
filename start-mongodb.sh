2688a3bc5a4cf89436c5f97bde74508d8c874c6a
Stefano De Vuono <io@stefanodevuono.com>
2020-03-10 19:18:15 -0700

    add replica set option

--- start-mongodb.sh
+++ start-mongodb.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-docker run --name mongodb --publish 27017:27017 --detach mongo:$1
+REPL_COMMAND=""
+
+if [ -n "$MONGO_REPL_SET" ]
+then
+    REPL_COMMAND="--replSet $MONGO_REPL_SET"
+fi
+
+docker run --name mongodb --publish 27017:27017 --detach mongo:$1 $REPL_COMMAND
+
+if [ -n "$MONGO_REPL_SET" ]
+then
+    sleep 1
+    echo "initiating $MONGO_REPL_SET."
+    docker exec -t mongodb mongo --eval "rs.initiate()"
+    echo "initiated"
+fi
