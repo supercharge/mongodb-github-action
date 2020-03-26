bcb6c4045eac703bf48496605c066c58482eb462
Marcus Poehls <marcus.poehls@gmail.com>
2020-03-26 13:08:29 +0100

    start MongoDB replica set when input is present

--- start-mongodb.sh
+++ start-mongodb.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-docker run --name mongodb --publish 27017:27017 --detach mongo:$1
+if [ -z "$MONGODB_REPLICA_SET" ]
+then
+  # empty replica set, start single MongoDB instance
+  docker run --name mongodb --publish 27017:27017 --detach mongo:$MONGODB_VERSION
+  return
+fi
+
+# Start with replica set
+docker run --name mongodb --publish 27017:27017 --detach mongo:$MONGODB_VERSION --replSet $MONGODB_REPLICA_SET
+
+echo "Initiating MongoDB replica set [$MONGODB_REPLICA_SET]"
+docker exec -t mongodb mongo --eval "rs.initiate()"
+echo "Check! Initiated MongoDB replica set [$MONGODB_REPLICA_SET]"
