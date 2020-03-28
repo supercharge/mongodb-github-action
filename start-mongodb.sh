4e71363138f9af56cbf0a3bd851f7eedeb6f19a6
Stefano De Vuono <io@stefanodevuono.com>
2020-03-27 19:11:24 -0700

    add sleep

--- start-mongodb.sh
+++ start-mongodb.sh
@@ -12,6 +12,7 @@ docker run --name mongodb --publish 27017:27017 --detach mongo:$1 $REPL_COMMAND
 if [ -n "$MONGO_REPL_SET" ]
 then
     echo "initiating $MONGO_REPL_SET."
+    sleep 1
     docker exec -t mongodb mongo --eval "rs.initiate()"
     echo "initiated"
 fi
