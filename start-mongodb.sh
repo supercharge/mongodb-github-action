40d8aca0da92a56b1e84dfa18ef4e91d01cd1340
Marcus Pöhls <marcus.poehls@gmail.com>
2020-04-09 11:41:31 +0200

    formatting

--- start-mongodb.sh
+++ start-mongodb.sh
@@ -30,5 +30,5 @@ do
 done
 
 echo "Initiating replica set [$MONGODB_REPLICA_SET]"
-docker exec --tty mongodb mongo --eval "rs.initiate(JSON.stringify({ _id: $MONGODB_REPLICA_SET, members: [ { _id: 0, host: localhost } ] }))"
+docker exec --tty mongodb mongo --eval "rs.initiate({ \"_id\": \"$MONGODB_REPLICA_SET\", \"members\": [{ \"_id\": 0, \"host\": \"localhost\" }] })"
 echo "Check! Initiated replica set [$MONGODB_REPLICA_SET]"
