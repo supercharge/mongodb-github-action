0827af347920d50a10e65d13f80501d482da6648
Marcus Pöhls <mp@seda.digital>
2021-05-11 09:17:47 +0200

    test on custom port

---
+++ test/custom-port.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Lab = require('@hapi/lab')
+const Mongoose = require('mongoose')
+const { expect } = require('@hapi/code')
+
+const { describe, it } = (exports.lab = Lab.script())
+
+describe('MongoDB Instance on Custom Port ->', () => {
+  it('connects to MongoDB on custom port 12345', async () => {
+    await expect(
+      Mongoose.connect('mongodb://localhost:12345', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+    ).to.not.reject()
+  })
+})
