d3ce5d142c27727e92249b4b3685b701a604256a
Marcus Pöhls <mp@seda.digital>
2021-06-07 11:22:32 +0200

    refine test

--- test/single-instance.js
+++ test/single-instance.js
@@ -4,36 +4,38 @@ const Lab = require('@hapi/lab')
 const Mongoose = require('mongoose')
 const { expect } = require('@hapi/code')
 
-const { describe, it } = (exports.lab = Lab.script())
+const { describe, it, afterEach } = (exports.lab = Lab.script())
 
 const { MONGODB_HOST = 'localhost', MONGODB_PORT = 27017 } = process.env
 
-describe('MongoDB Single Instance ->', () => {
-  const connectionString = `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/test`
+const connectionString = `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/test`
 
-  console.log('---------------------------------------------------------------------')
-  console.log('connecting to MongoDB using connection string -> ' + connectionString)
-  console.log('---------------------------------------------------------------------')
+console.log('---------------------------------------------------------------------')
+console.log('connecting to MongoDB using connection string -> ' + connectionString)
+console.log('---------------------------------------------------------------------')
+
+describe('MongoDB Single Instance ->', () => {
+  afterEach(async () => {
+    await Mongoose.disconnect()
+  })
 
   it('connects to MongoDB', async () => {
-    await expect(async () => {
-      const db = await Mongoose.connect(connectionString, {
+    await expect(
+      await Mongoose.connect(connectionString, {
         useNewUrlParser: true,
         useUnifiedTopology: true
       })
-
-      await db.disconnect()
-    }).to.not.reject()
+    ).to.not.reject()
   })
 
   it('fails to connect to non-existent MongoDB instance', async () => {
-    await expect(async () => {
+    await expect(
       await Mongoose.connect('mongodb://localhost:27018', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         connectTimeoutMS: 1000,
         serverSelectionTimeoutMS: 1000
       })
-    }).to.reject()
+    ).to.reject()
   })
 })
