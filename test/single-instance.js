3e4458246a29fd027dcdd4b4398e62b0e81388cb
Marcus Pöhls <mp@seda.digital>
2021-06-07 11:33:16 +0200

    use env vars for MongoDB host and port

--- test/single-instance.js
+++ test/single-instance.js
@@ -4,12 +4,24 @@ const Lab = require('@hapi/lab')
 const Mongoose = require('mongoose')
 const { expect } = require('@hapi/code')
 
-const { describe, it } = (exports.lab = Lab.script())
+const { describe, it, afterEach } = (exports.lab = Lab.script())
+
+const { MONGODB_HOST = 'localhost', MONGODB_PORT = 27017 } = process.env
+
+const connectionString = `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/test`
+
+console.log('---------------------------------------------------------------------')
+console.log('connecting to MongoDB using connection string -> ' + connectionString)
+console.log('---------------------------------------------------------------------')
 
 describe('MongoDB Single Instance ->', () => {
+  afterEach(async () => {
+    await Mongoose.disconnect()
+  })
+
   it('connects to MongoDB', async () => {
     await expect(
-      Mongoose.connect('mongodb://localhost', {
+      Mongoose.connect(connectionString, {
         useNewUrlParser: true,
         useUnifiedTopology: true
       })
@@ -18,7 +30,7 @@ describe('MongoDB Single Instance ->', () => {
 
   it('fails to connect to non-existent MongoDB instance', async () => {
     await expect(
-      Mongoose.connect('mongodb://localhost:27018', {
+      Mongoose.connect('mongodb://localhost:27018/test', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         connectTimeoutMS: 1000,
